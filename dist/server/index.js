b039d0df2aeeddc3cc4ba46ab14dc5159e544838
benjistiles <ben.stiles1228@gmail.com>
2026-09-05 15:07:04 -0400

    redesign

---
+++ dist/server/index.js
@@ -0,0 +1,13 @@
+const worker = {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+
+    if (url.pathname === "/") {
+      url.pathname = "/index.html";
+    }
+
+    return env.ASSETS.fetch(new Request(url, request));
+  },
+};
+
+export default worker;
